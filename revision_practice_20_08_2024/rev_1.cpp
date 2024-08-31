f3dbdf5d9bb785749684a01c4c726f6a9f09c836
NIKHILCODEBIT11 <nikhilcodebit11@gmail.com>
2024-08-31 12:14:34 +0530

    pushing before revision

---
+++ revision_practice_20_08_2024/rev_1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+int main(){
+    int a=12;
+    int b=7;
+    std::cout<<"The value of a is"<<a<<"and \n";
+    // std::cout<<'value of b is'<<'\n';
+    // std::cout<<b;
+    return 0;
+}
+
+/* Remember unlike python, in case of c++ :-
+
+   ' 'can't be used as " " in c++ 
+   */
